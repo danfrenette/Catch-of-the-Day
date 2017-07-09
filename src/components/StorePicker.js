7ae6d4d44cf40faf76d13ff22d4cc266762d8487
danfrenette <dan.r.frenette@gmail.com>
2017-07-09 12:15:47 -0400

    Add basic menu components

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,8 +3,8 @@ import React from "react";
 class StorePicker extends React.Component {
   render() {
     return (
-      <form className="store">
-        <h2> Please return a store</h2>
+      <form className="store-selector">
+        <h2>Please return a store</h2>
         <input type="text" required placeholder="Store Name"/>
         <button type="submit">Visit Store</button>
       </form>
