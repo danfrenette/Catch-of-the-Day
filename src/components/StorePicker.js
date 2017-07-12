b70c0672684be925c48e400723dd224c50d70728
danfrenette <dan.r.frenette@gmail.com>
2017-07-11 20:59:38 -0400

    Store picker submission takes the user to the store page

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,16 +2,30 @@ import React from "react";
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  constructor() {
+    super();
+    this.goToStore = this.goToStore.bind(this);
+  };
+
+  goToStore(e) {
+    e.preventDefault();
+    this.context.router.transitionTo(`/store/${this.storeInput.value}`)
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please return a store</h2>
         <input type="text" required placeholder="Store Name"
-        defaultValue={getFunName()}/>
+        defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
         <button type="submit">Visit Store</button>
       </form>
     )
   };
+};
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 };
 
 export default StorePicker;
