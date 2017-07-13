851c7964798d97156a07dd273eddad2c62d20b53
danfrenette <dan.r.frenette@gmail.com>
2017-07-12 22:03:53 -0400

    Add to Order button adds a fish to the order state

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
 
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
     // getInitialState
     this.state = {
       fishes: {},
@@ -31,6 +32,12 @@ class App extends React.Component {
     });
   }
 
+  addToOrder(key) {
+    const order = {...this.state.order};
+    order[key] = order[key] + 1 || 1;
+    this.setState({order: order});
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -38,9 +45,11 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
           <ul className="list-of-fishes">
             {
-              Object.
-                keys(this.state.fishes).
-                map(key => <Fish key={key} details={this.state.fishes[key]} />)
+              Object
+                .keys(this.state.fishes)
+                .map(key => <Fish key={key} index={key}
+                  details={this.state.fishes[key]}
+                  addToOrder={this.addToOrder}/>)
             }
           </ul>
         </div>
