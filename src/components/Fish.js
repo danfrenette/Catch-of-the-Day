6a4d0b4bc7977355e3872a0092cc8403d2117b95
danfrenette <dan.r.frenette@gmail.com>
2017-07-13 17:50:35 -0400

    Remove redundant HTML

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -8,7 +8,7 @@ class Fish extends React.Component {
     const buttonText = isAvailable ? "Add To Order" : "Sold Out!"
 
     return (
-      <li className="menu-fish">{this.props.details.name}
+      <li className="menu-fish">
         <img src={details.image} alt={details.name} />
         <h3 className="fish-name">
           {details.name}
