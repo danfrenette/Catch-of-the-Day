17ee0efd203f121aa7c1bd8ccd43ae2040d38793
danfrenette <dan.r.frenette@gmail.com>
2017-07-18 15:17:40 -0400

    Add proptypes to all components

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -3,7 +3,7 @@ import { formatPrice } from "../helpers";
 
 class Fish extends React.Component {
   render() {
-    const { details, index } = this.props;
+    const { details, index, addToOrder } = this.props;
     const isAvailable = details.status === "available";
     const buttonText = isAvailable ? "Add To Order" : "Sold Out!"
 
@@ -15,10 +15,16 @@ class Fish extends React.Component {
           <span className="price">{formatPrice(details.price)}</span>
         </h3>
         <p>{details.desc}</p>
-        <button onClick={() => this.props.addToOrder(index)} disabled={!isAvailable}>{buttonText}</button>
+        <button onClick={() => addToOrder(index)} disabled={!isAvailable}>{buttonText}</button>
       </li>
     )
   }
 }
+
+Fish.propTypes = {
+  details: React.PropTypes.object.isRequired,
+  index: React.PropTypes.string.isRequired,
+  addToOrder: React.PropTypes.func.isRequired
+};
 
 export default Fish;
